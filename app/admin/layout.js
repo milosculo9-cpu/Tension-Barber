e2e5d3d55b877cb0ea157ce92585541e2ab47a00
milosculo9-cpu <milosculo9@gmail.com>
2026-03-11 19:18:31 +0100

    Create layout.js

---
+++ app/admin/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Tension Barber - Admin Panel',
+  description: 'Admin panel za upravljanje terminima i rezervacijama',
+};
+
+export default function AdminLayout({ children }) {
+  return children;
+}
